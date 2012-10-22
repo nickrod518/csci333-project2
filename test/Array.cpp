f0e4551cd0d286a931452e366a120df88e46a5f0
Nick Rodriguez <nickrod518@gmail.com>
2012-10-22 00:45:25 -0400

    successfully added int tests for aarray

--- test/Array.cpp
+++ test/Array.cpp
@@ -8,3 +8,54 @@ TEST(AArray, Initialize) {
   EXPECT_EQ(30, a->getNumCols());
   delete a;
 }
+
+TEST(AArray, Insert) {
+  AArray<int>* a = new AArray<int>(5, 10, 1);
+  a->insert(1, 4, 0);
+  a->insert(0, 7, 200);
+
+  EXPECT_EQ(0, a->access(1, 4));
+  EXPECT_EQ(200, a->access(0, 7));
+}
+
+TEST(AArray, Access) {
+  AArray<int>* a = new AArray<int>(5, 10, 1);
+  a->insert(1, 4, 0);
+  a->insert(0, 7, 200);
+
+  EXPECT_EQ(1, a->access(0, 0));
+  EXPECT_EQ(1, a->access(4, 9));
+  EXPECT_EQ(1, a->access(4, 8));
+  EXPECT_EQ(0, a->access(1, 4));
+  EXPECT_EQ(200, a->access(0, 7));
+}
+
+TEST(AArray, Remove) {
+  AArray<int>* a = new AArray<int>(5, 10, 1);
+  a->insert(1, 4, 0);
+  a->insert(0, 7, 200);
+
+  a->remove(1, 4);
+  a->remove(0, 7);
+
+  EXPECT_EQ(1, a->access(1, 4));
+  EXPECT_EQ(1, a->access(0, 7));
+}
+
+TEST(AArray, Print) {
+
+}
+
+TEST(AArray, GetNumRows) {
+  int rows = 50;
+  AArray<int>* a = new AArray<int>(rows, 200, 0);
+
+  EXPECT_EQ(50, a->getNumRows());
+}
+
+TEST(AArray, GetNumCols) {
+  int cols = 95;
+  AArray<int>* a = new AArray<int>(80, cols, 0);
+
+  EXPECT_EQ(95, a->getNumCols());
+}
